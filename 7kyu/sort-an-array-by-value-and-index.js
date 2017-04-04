58000aa4ffcab6d4d85250fb353a3c5c86537078
Tomas Ruiz <tomastrg22@gmail.com>
2017-04-05 00:22:01 +0200

    sort-an-array-by-value-and-index added

---
+++ 7kyu/sort-an-array-by-value-and-index.js
@@ -0,0 +1,10 @@
+function sortByValueAndIndex(array) {
+  return array.map((item, index) => {
+    return [item, item * (index + 1)];
+  }).sort((a, b) => {
+    return a[1] > b[1] ? 1 : (a[1] < b[1] ? -1 : 0);
+
+  }).map(num => {
+    return num[0];
+  });
+}
